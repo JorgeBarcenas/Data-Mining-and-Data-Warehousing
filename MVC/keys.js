0a133f522f759258daf0cadc0ec9732d1e790ed8
JorgeBarcenas <50004342+JorgeBarcenas@users.noreply.github.com>
2019-04-28 18:27:51 -0500

    Delete keys.js

--- MVC/keys.js
+++
@@ -1,4 +0,0 @@
-//exports.conn='mongodb://root:toor@cluster0-shard-00-00-dzc5u.mongodb.net:27017,cluster0-shard-00-01-dzc5u.mongodb.net:27017,cluster0-shard-00-02-dzc5u.mongodb.net:27017/learning_mongo?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true';
-exports.conn='mongodb://root:toor@cluster0-shard-00-00-e5wzw.mongodb.net:27017,cluster0-shard-00-01-e5wzw.mongodb.net:27017,cluster0-shard-00-02-e5wzw.mongodb.net:27017/learning_mongo?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true';
-//exports.conn='mongodb://root:toor@cluster0-shard-00-00-e5wzw.mongodb.net:27017,cluster0-shard-00-01-e5wzw.mongodb.net:27017/learning_mongo?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true';
-//exports.conn='mongodb:root:toor@cluster0-shard-00-00-e5wzw.mongodb.net:27017,cluster0-shard-00-01-e5wzw.mongodb.net:27017,cluster0-shard-00-02-e5wzw.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true';
